4ddd070d9b569344ff667665d6a5bb391e2a3d02
augusto.savi <augusto.savi@betha.com.br>
2022-02-09 23:43:43 -0300

    Alterando o Repositório de categoria

--- src/modules/categories/controllers/index.ts
+++ src/modules/categories/controllers/index.ts
@@ -6,30 +6,22 @@ import { CreateCategoryController } from "./CreateCategoryController";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ListCategoriesController } from "./ListCategoriesController";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = new CategoriesRepository();
 
 // Criação de Category
 const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-const createCategoryController = new CreateCategoryController(
-    createCategoryService
-);
+export function createCategoryController(): CreateCategoryController {
+    return new CreateCategoryController(createCategoryService);
+}
 
 // Listagement des categories
 const listCategoriesService = new ListCategoriesService(categoriesRepository);
-
-const listCategoriesController = new ListCategoriesController(
-    listCategoriesService
-);
+export function listCategoriesController(): ListCategoriesController {
+    return new ListCategoriesController(listCategoriesService);
+}
 
 // Upload File
 const importCategoryService = new ImportCategoryService(categoriesRepository);
-const importCategoryController = new ImportCategoryController(
-    importCategoryService
-);
-
-export {
-    createCategoryController,
-    listCategoriesController,
-    importCategoryController,
-};
+export function importCategoryController(): ImportCategoryController {
+    return new ImportCategoryController(importCategoryService);
+}
--- src/modules/categories/routes/categories.routes.ts
+++ src/modules/categories/routes/categories.routes.ts
@@ -12,15 +12,15 @@ const categoriesRoutes = Router();
 const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (request, response) => {
-    return createCategoryController.handle(request, response);
+    return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-    return listCategoriesController.handle(request, response);
+    return listCategoriesController().handle(request, response);
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    importCategoryController.handle(request, response);
+    importCategoryController().handle(request, response);
 });
 
 export { categoriesRoutes };
